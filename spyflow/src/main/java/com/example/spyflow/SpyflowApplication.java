26809e9889b0b045d62afac5003e74aa01d448ca
okimin <danielgaston6@gmail.com>
2023-07-24 15:45:00 -0400

    Add Springboot initializer

---
+++ spyflow/src/main/java/com/example/spyflow/SpyflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.spyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyflowApplication.class, args);
+	}
+
+}
---
+++ spyflow/src/test/java/com/example/spyflow/SpyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
